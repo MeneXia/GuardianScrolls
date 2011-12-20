73ace319a3cf4257d8ea62c3dd76ec4675a0d0b9
Xavi Ablaza <xlablaza@gmail.com>
2011-12-21 00:14:58 +0800

    added in second commit, controls stackability better if spout is present

---
+++ GuardianScrollsCore/src/llhusnire/menexia/guardianscrolls/listeners/GSSpoutInventoryListener.java
@@ -0,0 +1,30 @@
+package llhusnire.menexia.guardianscrolls.listeners;
+
+import llhusnire.menexia.guardianscrolls.GuardianScrolls;
+
+import org.bukkit.Material;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.getspout.spoutapi.event.inventory.InventoryClickEvent;
+import org.getspout.spoutapi.event.inventory.InventoryListener;
+
+public class GSSpoutInventoryListener extends InventoryListener {
+	
+	public GSSpoutInventoryListener(GuardianScrolls plugin) {
+		plugin.getServer().getPluginManager().registerEvent(Event.Type.CUSTOM_EVENT, this,
+				Event.Priority.Monitor, plugin);
+	}
+	
+	@Override
+	public void onInventoryClick(InventoryClickEvent event) {
+		ItemStack itemClicked = event.getItem();
+		ItemStack itemHolding = event.getCursor();
+		if (itemClicked != null && itemClicked.getType() == Material.PAPER) {
+			if (itemHolding != null && itemHolding.getType() == Material.PAPER &&
+					itemClicked.getDurability() != itemHolding.getDurability()) {
+				event.setCancelled(true);
+			}
+		}
+	}
+
+}
